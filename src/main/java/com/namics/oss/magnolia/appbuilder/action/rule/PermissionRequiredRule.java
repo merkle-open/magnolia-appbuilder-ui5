17d4fc84d8cc5c23ed5ef68b74132413beee1efb
eschleb <bjoern.eschle@merkle.com>
2023-07-07 10:37:28 +0200

    Implement PermissionRequiredRule

---
+++ src/main/java/com/namics/oss/magnolia/appbuilder/action/rule/PermissionRequiredRule.java
@@ -0,0 +1,41 @@
+package com.namics.oss.magnolia.appbuilder.action.rule;
+
+import com.machinezoo.noexception.Exceptions;
+import info.magnolia.cms.security.PermissionUtil;
+import info.magnolia.ui.api.availability.AbstractAvailabilityRule;
+import info.magnolia.ui.vaadin.integration.jcr.JcrItemId;
+import info.magnolia.ui.vaadin.integration.jcr.JcrItemUtil;
+
+import javax.inject.Inject;
+import javax.jcr.Item;
+import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.RepositoryException;
+
+public class PermissionRequiredRule extends AbstractAvailabilityRule {
+	private final PermissionRequiredRuleDefinition definition;
+
+	@Inject
+	public PermissionRequiredRule(final PermissionRequiredRuleDefinition definition) {
+		this.definition = definition;
+	}
+
+	@Override
+	protected boolean isAvailableForItem(final Object itemId) {
+		if (itemId instanceof JcrItemId) {
+			return Exceptions.wrap().getAsBoolean(() -> {
+				final Item jcrItem = JcrItemUtil.getJcrItem((JcrItemId) itemId);
+				final Node node = getNode(jcrItem);
+				return PermissionUtil.isGranted(node, definition.getRequiredPermissions());
+			});
+		}
+		return false;
+	}
+
+	private Node getNode(final Item jcrItem) throws RepositoryException {
+		if(jcrItem instanceof Property){
+			return jcrItem.getParent();
+		}
+		return (Node) jcrItem;
+	}
+}
---
+++ src/main/java/com/namics/oss/magnolia/appbuilder/action/rule/PermissionRequiredRuleDefinition.java
@@ -0,0 +1,21 @@
+package com.namics.oss.magnolia.appbuilder.action.rule;
+
+import info.magnolia.ui.api.availability.AvailabilityRule;
+import info.magnolia.ui.api.availability.AvailabilityRuleDefinition;
+
+public class PermissionRequiredRuleDefinition implements AvailabilityRuleDefinition {
+	private final long requiredPermissions;
+
+	public PermissionRequiredRuleDefinition(final long requiredPermissions) {
+		this.requiredPermissions = requiredPermissions;
+	}
+
+	@Override
+	public Class<? extends AvailabilityRule> getImplementationClass() {
+		return PermissionRequiredRule.class;
+	}
+
+	public long getRequiredPermissions() {
+		return requiredPermissions;
+	}
+}
